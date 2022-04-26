95a6fb450b575f8ce64a1dd228ecb0340731428d
AdrianOrea <adriangomezorea1@gmail.com>
2022-04-26 16:46:14 +0200

    Principio templates

--- ieti/fct/models.py
+++ ieti/fct/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Abuelo(models.Model):
+    texto = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=150)
