2461ed61467ec04b8e66fe6e3eb438f32c85276a
Carlos Muñoz Rosendo <carmuny54@gmail.com>
2022-04-26 16:43:39 +0200

    monka

--- ieti/fct/models.py
+++ ieti/fct/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+from tinymce import models as tinymce_models
+
+class TipoNodo(models.Model):
+    nombre = models.CharField(max_length=50)
+
+
+
+class Nodo(models.Model):
+    tipo = models.ForeignKey('TipoNodo', on_delete=models.CASCADE)
+    texto= tinymce_models.HTMLField()
+    hijo = models.ManyToManyField('self', null=True, blank=True)
+    siguiente = models.ManyToManyField('self', null=True, blank=True)
+    ordern = models.IntegerField(default=1)
+
+
+
