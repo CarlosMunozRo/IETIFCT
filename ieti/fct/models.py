f89fad1616de09b3d3f0aa2410904fa26f821415
Carlos Muñoz <carmuny54@gmail.com>
2022-04-26 19:00:12 +0200

    abuelo

--- ieti/fct/models.py
+++ ieti/fct/models.py
@@ -4,17 +4,44 @@ from django.db import models
 
 from tinymce import models as tinymce_models
 
-class TipoNodo(models.Model):
+
+class NodoPadre(models.Model):
     nombre = models.CharField(max_length=50)
 
 
+    def __str__(self):
+        return self.nombre
 
 class Nodo(models.Model):
-    tipo = models.ForeignKey('TipoNodo', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+
+    nodoPadre = models.ForeignKey('NodoPadre',null=True,blank=True,on_delete=models.CASCADE)
+    nodoPadrePadre = models.ForeignKey('Nodo',null=True,blank=True,on_delete=models.CASCADE)
+
+    class TipoNodo(models.TextChoices):
+        PREGUNTA = 'PR', 'Pregunta'
+        SOLUCION = 'SO', 'Solucion'
+
+
+    tipo = models.CharField(
+        max_length=2,
+        choices=TipoNodo.choices,
+        default=TipoNodo.PREGUNTA,
+    )
+
     texto= tinymce_models.HTMLField()
-    hijo = models.ManyToManyField('self', null=True, blank=True)
-    siguiente = models.ManyToManyField('self', null=True, blank=True)
-    ordern = models.IntegerField(default=1)
+    orden = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nombre
 
 
+""" 
+class Orden(models.Model):
+    orden = models.IntegerField(default=1)
+    nodoPadre = models.ForeignKey('NodoPadre', on_delete=models.CASCADE)
+    nodo = models.ForeignKey('Nodo', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.orden) 
+"""
