9e0b8503a7ab434aa7867e8b05743b5b83cf5c83
Carlos Muñoz <carmuny54@gmail.com>
2022-05-04 18:18:08 +0200

    side bar functionality

--- ieti/fct/views.py
+++ ieti/fct/views.py
@@ -1,3 +1,5 @@
+from http.server import HTTPServer
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 from django.views.decorators.csrf import csrf_exempt
@@ -17,19 +19,38 @@ def buscar(nodos,nodoPadreId):
 
 def landingPage(request):
     contenido = Tema.objects.all()
+
+
     contexto = {
         'Contenidos':contenido,
     }
+
+
     return render(request,'landingPage.html',contexto)
 
 def pasos(request,temaid,pasoid):
-    tema = Tema.objects.get(id=temaid)
-    paso = Paso.objects.get(id=contenido.pasoInicial.id)
+
+
+    try:
+        tema = Tema.objects.get(id=temaid)
+    except:
+        return HttpResponse("No existe el tema")
+
+
+    try:
+        paso = Paso.objects.get(id=pasoid)
+    except:
+        return HttpResponse("El tema not tiene paso inicial")
+
+
     contexto = {
         'annotated_list': Paso.get_annotated_list(Paso.objects.get(id=tema.pasoInicial.id)),
+        'paso': paso,
+        'tema': tema,
     }
     return render(request,'wizard.html',contexto)
 
 
 def login(request):
 	return render(request, 'login.html')
+
